74ee1dae211b2c248c1f259889edf2af2eb627ef
AntidotRepository <haroldducept@gmail.com>
2018-05-07 16:14:47 +0200

    Display a cube

--- mywindow.cpp
+++ mywindow.cpp
@@ -7,12 +7,16 @@ myWindow::myWindow(QWidget *parent): myGLWidget(60, parent, "OpenGL learning..."
 
 void myWindow::initializeGL()
 {
+    f_x = 0.0;
     glShadeModel(GL_SMOOTH);
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glClearDepth(1.0f);
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+    loadTexture("res/box.png");
+    glEnable(GL_TEXTURE_2D);
 }
 
 void myWindow::resizeGL(int width, int height)
@@ -31,25 +35,59 @@ void myWindow::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
+
     glTranslatef(-1.5f, 0.0f, -6.0f);
-
-    glBegin(GL_TRIANGLES);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex3f(0.0f, 1.0f, 0.0f);
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(-1.0f, -1.0f, 0.0f);
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex3f(1.0f, -1.0f, 0.0f);
-    glEnd();
-
-    glTranslatef(3.0f, 0.0f, -6.0f);
+    glRotatef(f_x, 1.0, 0.3, 0.1);
 
     glBegin(GL_QUADS);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex3f(-1.0f, 1.0f, 0.0f);
-    glVertex3f(-1.0f, -1.0f, 0.0f);
-    glColor3f(0.5f, 0.5f, 0.5f);
-    glVertex3f(1.0f, -1.0f, 0.0f);
-    glVertex3f(1.0f, 1.0f, 0.0f);
+    // Face avant
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0, -1.0,  1.0f);
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0, -1.0,  1.0f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0,  1.0,  1.0f);
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0,  1.0,  1.0f);
+    // Face arrière
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0, -1.0, -1.0f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0,  1.0, -1.0f);
+    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0,  1.0, -1.0f);
+    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0, -1.0, -1.0f);
+    // Face haut
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0,  1.0, -1.0f);
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0,  1.0,  1.0f);
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0,  1.0,  1.0f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0,  1.0, -1.0f);
+    // Face bas
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0, -1.0, -1.0f);
+    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0, -1.0, -1.0f);
+    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0, -1.0,  1.0f);
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0, -1.0,  1.0f);
+    // Face droite
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0, -1.0, -1.0f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0,  1.0, -1.0f);
+    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0,  1.0,  1.0f);
+    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0, -1.0,  1.0f);
+    // Face gauche
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0, -1.0, -1.0f);
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0, -1.0,  1.0f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0,  1.0,  1.0f);
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0,  1.0, -1.0f);
     glEnd();
+}
+
+void myWindow::loadTexture(QString textureName)
+{
+    QImage qim_Texture;
+    QImage qim_TempTexture;
+    if(qim_TempTexture.load(textureName))
+    {
+        qim_Texture = QGLWidget::convertToGLFormat(qim_TempTexture);
+        glGenTextures(1, &texture[0]);
+        glBindTexture(GL_TEXTURE_2D, texture[0]);
+        glTexImage2D(GL_TEXTURE_2D, 0, 3, qim_Texture.width(), qim_Texture.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, qim_Texture.bits());
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+    else
+    {
+        std::cout<<"Could not load texture!";
+    }
 }
--- mywindow.h
+++ mywindow.h
@@ -2,6 +2,9 @@
 #define MYWINDOW_H
 
 #include "myglwidget.h"
+#include "QImage"
+#include <stdlib.h>
+#include <iostream>
 
 #ifdef Q_OS_MAC64
 #include <OpenGL/glu.h>
@@ -12,11 +15,16 @@
 class myWindow: public myGLWidget
 {
     Q_OBJECT
+private:
+    GLuint texture[1];
+    float f_x;
+
 public:
     explicit myWindow(QWidget *parent = 0);
     void initializeGL();
     void resizeGL(int width, int height);
     void paintGL();
+    void loadTexture(QString textureName);
 };
 
 #endif // MYWINDOW_H
